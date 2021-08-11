013d6f4f47b18d076e7f797d3853869b4a31712d
grippal <lucagrip@yahoo.com>
2021-08-11 19:07:21 -0400

    Build folder is now the docs folder

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,7 +7,7 @@ import postcss from "gulp-postcss";
 import atimport from "postcss-import";
 import tailwindcss from "tailwindcss";
 
-const SITE_ROOT = "./_site";
+const SITE_ROOT = "./docs";
 const POST_BUILD_STYLESHEET = `${SITE_ROOT}/assets/css/`;
 const PRE_BUILD_STYLESHEET = "./src/style.css";
 const TAILWIND_CONFIG = "./tailwind.config.js";
@@ -18,57 +18,56 @@ const jekyll = process.platform === "win32" ? "jekyll.bat" : "jekyll";
 const isDevelopmentBuild = process.env.NODE_ENV === "development";
 
 task("buildJekyll", () => {
-  browserSync.notify("Building Jekyll site...");
+    browserSync.notify("Building Jekyll site...");
 
-  const args = ["exec", jekyll, "build"];
+    const args = ["exec", jekyll, "build"];
 
-  if (isDevelopmentBuild) {
-    args.push("--incremental");
-  }
+    if (isDevelopmentBuild) {
+        args.push("--incremental");
+    }
 
-  return spawn("bundle", args, { stdio: "inherit" });
+    return spawn("bundle", args, { stdio: "inherit" });
 });
 
 task("processStyles", () => {
-  browserSync.notify("Compiling styles...");
+    browserSync.notify("Compiling styles...");
 
-  return src(PRE_BUILD_STYLESHEET)
-    .pipe(
-      postcss([
-        atimport(),
-        tailwindcss(TAILWIND_CONFIG),
-        ...(isDevelopmentBuild ? [] : [autoprefixer(), cssnano()]),
-      ])
-    )
-    .pipe(dest(POST_BUILD_STYLESHEET));
+    return src(PRE_BUILD_STYLESHEET)
+        .pipe(
+            postcss([
+                atimport(),
+                tailwindcss(TAILWIND_CONFIG),
+                ...(isDevelopmentBuild ? [] : [autoprefixer(), cssnano()]),
+            ])
+        )
+        .pipe(dest(POST_BUILD_STYLESHEET));
 });
 
 task("startServer", () => {
-  browserSync.init({
-    files: [SITE_ROOT + "/**"],
-    open: "local",
-    port: 4000,
-    server: {
-      baseDir: SITE_ROOT,
-      serveStaticOptions: {
-        extensions: ["html"],
-      },
-    },
-  });
+    browserSync.init({
+        files: [SITE_ROOT + "/**"],
+        open: "local",
+        port: 4000,
+        server: {
+            baseDir: SITE_ROOT,
+            serveStaticOptions: {
+                extensions: ["html"],
+            },
+        },
+    });
 
-  watch(
-    [
-      "**/*.css",
-      "**/*.html",
-      "**/*.js",
-      "**/*.md",
-      "**/*.markdown",
-      "!_site/**/*",
-      "!node_modules/**/*",
-    ],
-    { interval: 500 },
-    buildSite
-  );
+    watch(
+        [
+            "**/*.css",
+            "**/*.html",
+            "**/*.js",
+            "**/*.md",
+            "**/*.markdown",
+            "!_site/**/*",
+            "!node_modules/**/*",
+        ], { interval: 500 },
+        buildSite
+    );
 });
 
 const buildSite = series("buildJekyll", "processStyles");
